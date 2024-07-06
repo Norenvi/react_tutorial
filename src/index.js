64202dd069d7c8d19999e5b5ec1b800818754944
AntoineVigneron <antoine.vigneron@bcplatforms.com>
2024-07-06 16:57:53 +0200

    Third hour

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
-// Timecode : 1:27:20
+// Timecode : 2:27:20
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
 
-function Greeting() {
-    return (
-        <div>
-            <Person/>
-            <Message/>
-        </div>
-    );
-}
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-    return <p>this is my message</p>;
+const firstBook = {
+    author: 'Kristin Hannah',
+    title: 'The Women : A Novel',
+    img: 'https://m.media-amazon.com/images/I/913C+MR3S5L._SL1500_.jpg'
 };
 
-// function Greeting() {
-//     return React.createElement('h2',{}, 'hello world');
-// }
+const secondBook = {
+    author: 'Lauren Roberts',
+    title: 'Reckless (The Powerless Trilogy)',
+    img: 'https://m.media-amazon.com/images/I/81q6ecxcZUL._SL1500_.jpg'
+};
+
+const BookList = () => {
+    return <section className='booklist'>
+        <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
+        <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
+    </section>
+}
+
+const Book= ({img, title, author}) => {
+    return ( 
+    <article className='book'>
+        <img src={img} alt={title}/>
+        <h2>{title}</h2>
+        <h2>{author}</h2>
+    </article>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting/>);
+root.render(<BookList/>);
