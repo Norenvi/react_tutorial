7b39afa341c9cf1a50a98d612c7c60728eac36fd
AntoineVigneron <antoine.vigneron@bcplatforms.com>
2024-08-03 17:48:20 +0200

    Final commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/91mP3553EzL._SL1500_.jpg';
+import img2 from './images/81q6ecxcZUL._SL1500_.jpg';
+import img3 from './images/81O6JfipQhL._SL1500_.jpg';
+
+export const books = [
+    {
+        author: 'Kristin Hannah',
+        title: 'The Women : A Novel',
+        img: img1,
+        id: 1,
+    },
+    {
+        author: 'Lauren Roberts',
+        title: 'Reckless (The Powerless Trilogy)',
+        img: img2,
+        id: 2,
+    },
+    {
+        author: 'Alex Hirsch',
+        title: 'The Book of Bill (Gravity Falls)',
+        img: img3,
+        id: 3,
+    },
+]
--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
-// Timecode : 2:27:20
+// DONE
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
-
-const firstBook = {
-    author: 'Kristin Hannah',
-    title: 'The Women : A Novel',
-    img: 'https://m.media-amazon.com/images/I/913C+MR3S5L._SL1500_.jpg'
-};
-
-const secondBook = {
-    author: 'Lauren Roberts',
-    title: 'Reckless (The Powerless Trilogy)',
-    img: 'https://m.media-amazon.com/images/I/81q6ecxcZUL._SL1500_.jpg'
-};
+import {books} from './books'
+import Book from './Book'
 
 const BookList = () => {
-    return <section className='booklist'>
-        <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
-        <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
-    </section>
+    return (
+        <div>
+            <h1>Amazon best sellers</h1>
+            <section className='booklist'>
+                {books.map((book, index) => {
+                    const {img, title, author, id} = book
+                    return <Book {...book} key={book.id} number={index}/>
+                })}
+            </section>
+        </div>
+    )
 }
 
-const Book= ({img, title, author}) => {
-    return ( 
-    <article className='book'>
-        <img src={img} alt={title}/>
-        <h2>{title}</h2>
-        <h2>{author}</h2>
-    </article>
-    );
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
